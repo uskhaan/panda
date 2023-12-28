939de5dd9e61811403df200ae74b2679bca31201
“uskhaan” <51319106+uskhaan@users.noreply.github.com>
2023-12-28 23:28:40 +0500

    skill universe added

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+      <Layout className=" py-8 flex items-center justify-between ">
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+
+        <div className="flex items-center">
+          Developed with
+          {/* <span className="text-primary text-2xl px-1">&#9825;</span> */}
+          <span className="text-2xl px-1">🎯</span>
+          by&nbsp;
+          <Link href="/" className="underline underline-offset-2">
+            Umar
+          </Link>
+        </div>
+        {/* <Link href="/">Say hello</Link> */}
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
