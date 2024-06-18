cdeb921c29a063f5ea02d13332690867c2a50646
“uskhaan” <117968231+uskhaan-wateen@users.noreply.github.com>
2024-06-19 00:00:35 +0500

    cv link change

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="https://drive.google.com/file/d/1j49mJHRMWSc4TIC6W73qW90QRswooGkq/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1mPwMzkqVdx_5sO584gT8rAnfhu9T1_59/view?usp=drive_link"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
                 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base
